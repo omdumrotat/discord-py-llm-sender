e53a5ab920f45baf7d2b084a788343ec89659569
om rotat <79361758+omdumrotat@users.noreply.github.com>
2024-05-11 20:48:52 +0700

    Update legitimate.py

--- legitimate.py
+++ legitimate.py
@@ -23,13 +23,13 @@ def check_discord_py_version(): #itll return true regardless lma
         return True
 def check_openai():
     try:
-        import openai as OpenAI
+        from openai import OpenAI
         return True
     except ImportError:
         print("OpenAI Module is not installed. Installing OpenAI with pip...")
         os.system("pip install -U openai")
         print("OpenAI installed successfully.")
-        import openai as OpenAI
+        from openai import OpenAI
         return True
 def token():
     try:
